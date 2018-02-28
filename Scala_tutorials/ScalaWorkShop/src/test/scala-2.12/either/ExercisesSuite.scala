9ab9fa17c6f3c718a63fd1ce11fdb636cf1326fb
Peter Hajdu <peter.hajdu@balabit.com>
2018-02-28 16:47:26 +0100

    add some more either exercises

--- Scala_tutorials/ScalaWorkShop/src/test/scala-2.12/either/ExercisesSuite.scala
+++ Scala_tutorials/ScalaWorkShop/src/test/scala-2.12/either/ExercisesSuite.scala
@@ -5,31 +5,45 @@ import org.scalatest.{FreeSpec, Matchers}
 class ExercisesSuite extends FreeSpec with Matchers {
   import Exercises._
 
-  "half age" - {
-    val users: List[User] =
-      List(
-        User(1, "Adam", None),
-        User(2, "Joe", Some(40)),
-        User(3, "Jeff", Some(70)),
-        User(9, "Sarah", None),
-        User(10, "Bill", Some(120)),
-        User(12, "Ivan", Some(32))
-      )
+  val users: List[User] =
+    List(
+      User(1, "Adam", None),
+      User(2, "Joe", Some(40)),
+      User(3, "Jeff", Some(70)),
+      User(9, "Sarah", None),
+      User(10, "Bill", Some(120)),
+      User(12, "Ivan", Some(32))
+    )
 
-    "should return half of the given user's age" in {
-      halfAge(users, 2) shouldBe Right(20)
+  "half age" - {
+    "should return the given user's age" in {
+      ageOf(users, 2) shouldBe Right(40)
     }
 
     "should return UnknownUser if there is no user with the given id" in {
-      halfAge(users, 4) shouldBe Left(UnknownUser)
+      ageOf(users, 4) shouldBe Left(UnknownUser)
     }
 
     "should return UnknownAge if the age of the given user is unknonwn" in {
-      halfAge(users, 9) shouldBe Left(UnknownAge)
+      ageOf(users, 9) shouldBe Left(UnknownAge)
     }
 
     "should work with empty lists" in {
-      halfAge(Nil, 2) shouldBe Left(UnknownUser)
+      ageOf(Nil, 2) shouldBe Left(UnknownUser)
+    }
+  }
+
+  "tell lucky number" - {
+    "should return half of the given user's age" in {
+      tellLuckyNumber(users, 2) should include("20")
+    }
+
+    "should return UnknownUser if there is no user with the given id" in {
+      tellLuckyNumber(users, 4) should include("you are unlucky")
+    }
+
+    "should return UnknownAge if the age of the given user is unknonwn" in {
+      tellLuckyNumber(users, 9) should include("you are unlucky")
     }
   }
 }
